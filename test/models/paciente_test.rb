a5fded0e631284e70a9e6a00861624ea34189b27
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-09-06 11:26:36 -0300

    realizando os testes de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criando paciente corretamente" do
+    paciente = Paciente.new nome: "Jonathan", data_nascimento: "06-06-1990", cpf: "706.351.254-47",
+                            email: "jonathan2018.matos@gmail.com"
+
+    assert paciente.save!
+  end
+
+  test "criando paciente com data de nascimento incorreta" do
+    paciente = Paciente.new nome: "Jonathan", data_nascimento: "06-06-2023", cpf: "706.351.254-47",
+                            email: "jonathan2018.matos@gmail.com"
+
+    assert_not paciente.save
+  end
 end
