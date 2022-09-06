b010cbb3eb4774aefe3494cac96812019d91568a
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-09-06 13:30:02 -0300

    fazendo testes para consulta

--- test/models/consulta_test.rb
+++ test/models/consulta_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 class ConsultaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "criando consulta" do
+    medico = Medico.new nome: 'Chooper o Adorador de Algodao Doce', cpf: '706.351.254-47', email: 'chooper@rodrigo.com',
+                        especialidade: 'Clinico Geral', crm: '123456'
+    assert medico.save
+
+    paciente = Paciente.new nome: "Jonathan", data_nascimento: "06-06-1990", cpf: "706.351.254-47",
+                            email: "jonathan2018.matos@gmail.com"
+    assert paciente.save
+
+    consulta = Consulta.new data: "13-09-2022", horario: "15:00", medico_id: medico.id, paciente_id: paciente.id
+
+    assert consulta.save
+  end
+
+  test "criando consulta com horario invalido" do
+    medico = Medico.new nome: 'Chooper o Adorador de Algodao Doce', cpf: '706.351.254-47', email: 'chooper@rodrigo.com',
+                        especialidade: 'Clinico Geral', crm: '123456'
+    assert medico.save
+
+    paciente = Paciente.new nome: "Jonathan", data_nascimento: "06-06-1990", cpf: "706.351.254-47",
+                            email: "jonathan2018.matos@gmail.com"
+    assert paciente.save
+
+    consulta = Consulta.new data: "13-09-2022", horario: "12:00", medico_id: medico.id, paciente_id: paciente.id
+
+    assert_not consulta.save
+  end
+
+  test "criando consulta com paciente invalido" do
+    medico = Medico.new nome: 'Chooper o Adorador de Algodao Doce', cpf: '706.351.254-47', email: 'chooper@rodrigo.com',
+                        especialidade: 'Clinico Geral', crm: '123456'
+    assert medico.save
+
+    paciente = Paciente.new nome: "Jonathan", data_nascimento: "06-06-1990", cpf: "706.351.254-47",
+                            email: "jonathan2018.matos@gmail.com"
+
+    consulta = Consulta.new data: "13-09-2022", horario: "11:00", medico_id: medico.id, paciente_id: paciente.id
+
+    assert_not consulta.save
+  end
 end
