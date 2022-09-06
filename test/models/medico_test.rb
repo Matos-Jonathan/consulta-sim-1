42e785face5ec14751583c8bd74dd9d3279d0618
Matos-Jonathan <jonathan2018.matos@gmail.com>
2022-09-05 22:29:57 -0300

    Realizando os testes do model medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -2,8 +2,30 @@ require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
   test 'criando medico corretamente' do
-    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@rodrigo.com',
-                        especialidade: 'Reumatologista', crm: '123456'
+    medico = Medico.new nome: 'Chooper o Adorador de Algodao Doce', cpf: '706.351.254-47', email: 'chooper@rodrigo.com',
+                        especialidade: 'Clinico Geral', crm: '123456'
+    assert medico.save
+  end
+
+  test 'criando medico com email vazio' do
+    medico = Medico.new nome: 'Chooper o Adorador de Algodao Doce', cpf: '706.351.254-47', email: ' ',
+                        especialidade: 'Clinico Geral', crm: '123456'
+    assert_not medico.save
+  end
+
+  test 'atualizando email incorretamente' do
+    medico = Medico.new nome: 'Chooper o Adorador de Algodao Doce', cpf: '706.351.254-47', email: 'chooper@rodrigo.com',
+                        especialidade: 'Clinico Geral', crm: '123456'
+
+    medico.email = " "
+    assert_not medico.save
+  end
+
+  test 'atualizando especialidade corretamente' do
+    medico = Medico.new nome: 'Chooper o Adorador de Algodao Doce', cpf: '706.351.254-47', email: 'chooper@rodrigo.com',
+                        especialidade: 'Clinico Geral', crm: '123456'
+
+    medico.especialidade  = 'Cirurgão Geral'
     assert medico.save
   end
 end
